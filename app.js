143dfc031b6543ca3e82f194741edfc42f51a75f
oni oluwatosin <oluwatosin.dev@gmail.com>
2025-10-13 11:18:25 +0100

    new route

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ const app = express();
 app.use(express.json());
 
 //routes
-
+app.get('/', (req, res, next) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'Server is currently running....',
+  });
+});
 app.use('/api/v1/users', userRouter);
 
 module.exports = app;
