913f691dc35fedba6195845f3bb147f704818587
oni oluwatosin <oluwatosin.dev@gmail.com>
2025-10-14 21:40:29 +0100

    CORS

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ const corsOptions = {
   origin: [
     'http://localhost:5173',
     'http://coach-x.vercel.app',
-    'https://coach-x.vercel.app/',
+    'https://coach-x.vercel.app',
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
